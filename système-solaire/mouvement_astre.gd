ea54e22d54d91de14753c75efceb66db9ce5cedd
Alexis <agagnon2007@icloud.com>
2026-04-16 10:11:07 -0400

    correction runge-kutta et début accélération

--- système-solaire/mouvement_astre.gd
+++ système-solaire/mouvement_astre.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 @export var centre_rotation: RigidBody3D
 @export var lune: RigidBody3D
-
+@export var autres_corps: Array[Node3D]
 
 @export_group("élements orbitaux")
 @export var demi_grand_axe: float
@@ -30,7 +30,7 @@ extends RigidBody3D
 @export var periode_relative: float = 20.0
 @export var etapes_calcul_par_ecran: int = 10000
 
-const masse_corps_rotation :float =1.9884e30
+const masse_soleil :float =1.9884e30
 const G: float = 6.673e-11
 
 var distance_entre_astre: Vector3
@@ -72,13 +72,7 @@ func conv_position_reelle_a_simulee(position_reelle : Vector3) -> Vector3:
 
 
 
-
-func force_attraction_entre_corps():
-	var a_c= G*(masse*masse)/(distance_entre_astre.length()**3) * distance_entre_astre
-
-
-
-func calculer_acceleration_gravitationnelle(position_rellee: Vector3) -> Vector3:
+func acceleration(position_rellee: Vector3) -> Vector3:
 	"""Calcule l'accélération gravitationnelle exercée sur le corps selon sa position
 	
 	Paramètre:
@@ -87,31 +81,38 @@ func calculer_acceleration_gravitationnelle(position_rellee: Vector3) -> Vector3
 	Retour:
 	L'accélération gravitationnelle exercée sur le corps en m/s^2
 	"""
-	var facteur = -G * masse * masse_corps_rotation / (position_rellee.length()**3)
-	var force = (position_rellee - centre_rotation.position) * facteur
-	return force / masse
+	var force: Vector3
+	for corps in autres_corps:
+		
+		var facteur = -G * masse_soleil / (corps.position_rellee.length()**3)
+		force = (position_rellee - centre_rotation.position) * facteur
+	return force
 func runge_kotta(temps_dernier_ecran):
 		#Nombre de période à simuler dans l'écran
 	var nb_periode = temps_dernier_ecran  * periode / periode_relative
 	#Pas de la simulation
-	var h = nb_periode / etapes_calcul_par_ecran
+	var dt = nb_periode / etapes_calcul_par_ecran
 		
 	for i in range(etapes_calcul_par_ecran):
-		a_i = calculer_acceleration_gravitationnelle(r_i)
-		
-		
-		k1 = r_i + h * v_i
-		k2= r_i+ h/2*k1
-		k3= r_i+ h/2*k2
-		k4= r_i+h*k3
-		r_i= r_i+(1/6*h*(k1+2*k2+2*k3+k4))
-		
-		k1 = v_i + h * v_i
-		k2= v_i+ h/2*k1
-		k3= v_i+ h/2*k2
-		k4= v_i+h*k3
-		v_i= v_i+(1/6*h*(k1+2*k2+2*k3+k4))
-		
+		# k1
+		var k1_r = v_i
+		var k1_v = acceleration(r_i)
+
+		# k2
+		var k2_r = v_i + 0.5 * dt * k1_v
+		var k2_v = acceleration(r_i + 0.5 * dt * k1_r)
+
+		# k3
+		var k3_r = v_i + 0.5 * dt * k2_v
+		var k3_v = acceleration(r_i + 0.5 * dt * k2_r)
+
+		# k4
+		var k4_r = v_i + dt * k3_v
+		var k4_v = acceleration(r_i + dt * k3_r)
+
+		# Mise à jour finale
+		r_i += (dt / 6.0) * (k1_r + 2.0*k2_r + 2.0*k3_r + k4_r)
+		v_i += (dt / 6.0) * (k1_v + 2.0*k2_v + 2.0*k3_v + k4_v)
 		
 		
 		
@@ -124,4 +125,5 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	position = conv_position_reelle_a_simulee(runge_kotta(etapes_calcul_par_ecran))
+	runge_kotta(etapes_calcul_par_ecran)
+	position = conv_position_reelle_a_simulee(r_i)
